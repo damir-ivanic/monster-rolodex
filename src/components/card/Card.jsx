19fd6ed3255c61b3b1170860d46ce632cf8deffa
damirfe <damir.edu@gmail.com>
2019-12-04 21:35:01 +0100

    Added factor of random pictures

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ monster }) => {
     <div className="card-container">
       <img
         alt="moster"
-        src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+        src={`https://robohash.org/${Math.random()}?set=set2&size=180x180`}
       />
       <h2>{monster.name}</h2>
       <p>{monster.email}</p>
