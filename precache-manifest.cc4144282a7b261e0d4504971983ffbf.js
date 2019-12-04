14d92e1fb6512ef8f8ae19019ff48d86b12f67b8
damirfe <damir.edu@gmail.com>
2019-12-04 20:33:29 +0100

    Updates

---
+++ precache-manifest.cc4144282a7b261e0d4504971983ffbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981c62fc5c0ec8ee2984d0ec9c194c31",
+    "url": "/damir-ivanic/monster-rolodex/index.html"
+  },
+  {
+    "revision": "de5703ef692d71197443",
+    "url": "/damir-ivanic/monster-rolodex/static/css/main.2422eac3.chunk.css"
+  },
+  {
+    "revision": "622eb90077062a5b0de2",
+    "url": "/damir-ivanic/monster-rolodex/static/js/2.98d8564b.chunk.js"
+  },
+  {
+    "revision": "de5703ef692d71197443",
+    "url": "/damir-ivanic/monster-rolodex/static/js/main.2620e90b.chunk.js"
+  },
+  {
+    "revision": "a4c72f25211561c218d6",
+    "url": "/damir-ivanic/monster-rolodex/static/js/runtime-main.5f67a8db.js"
+  }
+]);
